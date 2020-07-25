69ec62d4c141b9c2bbadaa9cdffe939bc8151353
niallmartinryan <ryann11@tcd.ie>
2020-07-25 14:18:44 +0100

    adding relevant comments and cleaning up code

--- src/main/java/com/NR/UrlShortener/URLShortenerResource.java
+++ src/main/java/com/NR/UrlShortener/URLShortenerResource.java
@@ -9,34 +9,45 @@ import org.springframework.web.servlet.view.*;
 import java.nio.charset.StandardCharsets;
 import com.google.common.hash.Hashing;
 
+// Entry Point
 @RequestMapping("/rest/url")
 @RestController
 public class URLShortenerResource{
+    //Redis Db template
     @Autowired
     StringRedisTemplate redisTemp;
-    // Jedis jedis;
+    
 
+    // Get request with short URL
     @GetMapping("/{id}")
     public RedirectView getLongUrl(@PathVariable String id){
 
+        // retrieving long url from redis dict
         String url = redisTemp.opsForValue().get(id);
         System.out.println("URL Returned: " + url);
+
+        // null exception protection if URL does not exist
         if (url == null) {
             throw new RuntimeException("Short URL does not exist for : " + id);
         }
         
+        // redirect user to long url
         return new RedirectView(url);
-        // return url;
+        
     }
-
+    // Post request Long URL
     @PostMapping
     public String createID(@RequestBody String url){
         
+        //Validation of Long url
         UrlValidator urlValidator = new UrlValidator( new String[]{"http", "https"});
 
         if(urlValidator.isValid(url)){
+            // Using Murmur hash to get short url which is id
             String id = Hashing.murmur3_32().hashString(url, StandardCharsets.UTF_8).toString();
             System.out.println("Id created: " + id);
+            
+            // insert short url (id) and long url (url) into db as keyset
             redisTemp.opsForValue().set(id,url);
 
             return id;
